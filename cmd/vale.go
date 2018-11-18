c249ed15f56443d12068ccbf89a64db1046cea6d
Chris Ward <chriswhward@gmail.com>
2018-11-18 15:16:09 +0100

    Format code

--- cmd/vale.go
+++ cmd/vale.go
@@ -34,9 +34,9 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 Run: func(cmd *cobra.Command, args []string) {
 	   if len(args) > 0 {
-		runVale("testthedocs/ttd-vale " + args[0])
+		   runVale("testthedocs/ttd-vale " + args[0])
         } else {
-					runVale("testthedocs/ttd-vale")
+			runVale("testthedocs/ttd-vale")
 		}
 	},
 }
